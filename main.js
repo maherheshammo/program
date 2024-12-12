9e20c35994a523751bc510947f1080f902d1dad3
maherheshammo <maherhesham52134@gmail.com>
2024-12-12 13:19:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let photo = document.querySelector(".imgs")
+let body = document.querySelector("body")
+
+function btn(scrr){
+photo.src = scrr
+}
+function colr(cl){
+    body.style.background = cl
+    
+}
